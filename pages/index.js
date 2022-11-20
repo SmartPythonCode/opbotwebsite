6109e3635a0e09912795a79cad91e01eafc861ec
SmartPythonCode <114207244+SmartPythonCode@users.noreply.github.com>
2022-11-20 21:04:47 +1300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Op Bot A Multipurpose Bot!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Op Bot The Best Modified Version Of Miltaro Bot!</code>
         </p>
       </main>
 
